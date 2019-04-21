a9fe671388e83da3f86f40a2fdaa02571a2275bb
DrBuschle <dr.buschle@hotmail.com>
2019-04-21 23:59:10 +0200

    R Path selection

---
+++ Set_a_path/Automatically_OS_independently/Set_a_path_manually.R
@@ -0,0 +1,4 @@
+# set a path automatically, operating system independently from the clipboard
+
+my_path <- readClipboard()  # reads path from clipboard
+
---
+++ Set_a_path/Manually_OS_dependently/Set_a_path_manually.R
@@ -0,0 +1,4 @@
+# set a path manually operating system specifically
+
+setwd("your/path")  # Unix
+setwd("your\path")  # Windows

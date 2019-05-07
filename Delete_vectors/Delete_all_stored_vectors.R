85049ae7a6a989482ea246ec13aac4e64232166e
DrBuschle <dr.buschle@hotmail.com>
2019-05-07 13:47:33 +0200

    how to remove files

---
+++ Delete_vectors/Delete_all_stored_vectors.R
@@ -0,0 +1,3 @@
+# remove all stored values
+
+rm(list = ls())
---
+++ Delete_vectors/Delete_single vectors.R
@@ -0,0 +1,7 @@
+# delete single vectors
+
+x <- c(1, 2, 3) # create vector
+x   # print vector
+
+rm(x) # remove vector
+x   # print vector -> does not exist any more

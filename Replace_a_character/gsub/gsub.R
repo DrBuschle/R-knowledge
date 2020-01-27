fafd2b86205dbcb31d6fe0b8db47c7a1b9ef1287
DrBuschle <dr.buschle@hotmail.com>
2020-01-27 13:44:41 +0100

    Newest_updates

--- Replace_a_character/gsub/gsub.R
+++ Replace_a_character/gsub/gsub.R
@@ -2,3 +2,11 @@
 
 gsub("pattern", "replacement", "yourpattern")
 
+# pattern
+.	# universal character recognizing everything
+*	# repeating the last character indefinately
+^	# starting with
+$	# ending with
+
+# for further info see regular expression git repository
+

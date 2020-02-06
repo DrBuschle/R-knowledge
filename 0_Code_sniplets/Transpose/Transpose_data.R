fc1289be7425dfef0d2c03d34c5d0391b0c1e86a
DrBuschle <dr.buschle@hotmail.com>
2020-02-06 14:56:01 +0100

    Newest_updates

--- 0_Code_sniplets/Transpose/Transpose_data.R
+++ 0_Code_sniplets/Transpose/Transpose_data.R
@@ -1,9 +1,12 @@
 # transpose a table/ data frame
 
 
+# prepare example
+
 Example_data <- data.frame(c(1:5), c(6:10))
 
 rownames(Example_data) <- c("one", "two", "three", "four", "five")  # add rownames for the sake of clarity
 colnames(Example_data) <- c("col_one", "col_two") # add colnames for the sake of clarity
 
+# transpose data
 t(Example_data)

a798c7d5b01e146b8b4087a4ad22a2db43947325
DrBuschle <dr.buschle@hotmail.com>
2020-02-06 14:53:00 +0100

    Newest_updates

--- 0_Code_sniplets/Transpose/Transpose_data.R
+++ 0_Code_sniplets/Transpose/Transpose_data.R
@@ -2,6 +2,8 @@
 
 
 Example_data <- data.frame(c(1:5), c(6:10))
-rownames(Example_data) <- c("one", "two", "three", "four", "five")
-colnames(Example_data) <- c("col_one", "col_two")
+
+rownames(Example_data) <- c("one", "two", "three", "four", "five")  # add rownames for the sake of clarity
+colnames(Example_data) <- c("col_one", "col_two") # add colnames for the sake of clarity
+
 t(Example_data)

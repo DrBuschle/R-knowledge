cc2fedbcae8557bffeedfc1552e295a3d0ac262c
DrBuschle <dr.buschle@hotmail.com>
2020-02-25 13:19:10 +0100

    Newest_updates

---
+++ 0_Code_sniplets/Mathematical_calculations/Mean/Mean.R
@@ -0,0 +1,9 @@
+# calculate the mean
+
+
+mean(c(1,2,3,4,5))  # calculates the mean, don't forget the c(), otherwise you get back the first number as a wrong result
+
+
+
+### !!! DON'T do it like THIS !!!:
+mean(8,9,10)

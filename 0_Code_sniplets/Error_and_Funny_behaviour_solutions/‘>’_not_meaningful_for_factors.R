8b2b10bed6c78b04b57148476675dfaa3bf2c6de
DrBuschle <dr.buschle@hotmail.com>
2020-03-19 14:22:02 +0100

    Newest_updates

--- 0_Code_sniplets/Error_and_Funny_behaviour_solutions/‘>’_not_meaningful_for_factors.R
+++ 0_Code_sniplets/Error_and_Funny_behaviour_solutions/‘>’_not_meaningful_for_factors.R
@@ -1,25 +1,4 @@
-# In case you get numbers instead of original text or numbers 
-# you probably have a problem with factors
+# ‘>’_not_meaningful_for_factors
 
-
-# example data
-
-# numeric looking factors
-a <- as.factor(100:104)
-
-# transform to numeric causes the problem that factor levels are returned
-as.numeric(a)
-
-# -> Solution
-as.numeric(as.character(a))
-
-
-
-
-
-# Problem does not occur for factors looking like characters
-
-# character looking factors
-b <- as.factor(c("Hell", "World", "it", "is", "me"))
-as.character(b)
-
+# solution: use stringsAsFactors = F while reading file
+data.frame(read.table("raw_data.txt", dec = ",", stringsAsFactors = F))

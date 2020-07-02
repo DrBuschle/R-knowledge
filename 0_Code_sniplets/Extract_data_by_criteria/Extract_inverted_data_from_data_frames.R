51f296f3bda0d8995d5b149c7d2af97536f51603
DrBuschle <dr.buschle@hotmail.com>
2020-07-02 19:26:20 +0200

    Newest_updates

---
+++ 0_Code_sniplets/Extract_data_by_criteria/Extract_inverted_data_from_data_frames.R
@@ -0,0 +1,16 @@
+# Extract data by criteria
+
+Example_data <- data.frame(cbind(c(1, 2, 3, 4), c(1, 2, 3, 4), c(1, 2, 3, 4), c(1, 2, 3, 4)), row.names = c("a", "b", "c", "d"))
+
+# select a specific set of data
+
+Example_data[c(1,3), ]
+
+
+# select the opposide set of data
+
+Example_data[-c(1,3), ]
+# or
+Example_data[c(2,4), ]
+
+

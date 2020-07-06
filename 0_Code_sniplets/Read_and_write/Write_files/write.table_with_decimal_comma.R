972090fd554276878e73d03b5d9c019893aef993
DrBuschle <dr.buschle@hotmail.com>
2020-07-06 22:57:07 +0200

    Newest_updates

---
+++ 0_Code_sniplets/Read_and_write/Write_files/write.table_with_decimal_comma.R
@@ -0,0 +1,8 @@
+# write data to a table
+
+write.table(vector_containing_your_data, 'Filename.ending', sep = "\t", quote = F, col.names = F, row.names = F, dec = ",")
+
+# sep can be choosen between "\t" (tab delimited), ",", ";", or what ever you desire
+# quote = F makes sure no quotes are added to all your data
+# col.names = F and row.names = F makes sure these are not written to the output
+# dec = ",": write decimal separator as "," instead of "."

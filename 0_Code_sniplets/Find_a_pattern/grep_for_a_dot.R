122fa6f2be7de4c62a29cd5f585d9082d18b16c0
DrBuschle <dr.buschle@hotmail.com>
2020-07-23 18:02:23 +0200

    Newest_updates

---
+++ 0_Code_sniplets/Find_a_pattern/grep_for_a_dot.R
@@ -0,0 +1,9 @@
+# find a dot with grep
+
+Example_names <- c("Hello_(World).txt", "Grandma_hello", "Daddy_Hello.txt")
+
+
+# find all the names including a dot
+grep("\\.", Example_names)
+
+
--- 0_Code_sniplets/Find_a_pattern/grep_for_parenthesis.R
+++ 0_Code_sniplets/Find_a_pattern/grep_for_parenthesis.R
@@ -1,9 +1,9 @@
-# find a pattern with grep
+# find pq43nthesis with grep
 
 Example_names <- c("Hello_(World).txt", "Grandma_hello.txt", "Daddy_Hello.txt")
 
 
-# find all the names including "Hello" - case sensitive!!!
+# find all the names including "(Wolrd)" - case sensitive!!!
 grep("\\(World\\)", Example_names)
 
 
